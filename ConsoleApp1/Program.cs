e58a12107dcbaffc2ba0d2174633fe1e323e45f1
kirill1234322 <kirilltrofimenko757@gmail.com>
2022-01-24 17:13:49 +0500

    dobavilVetkuMultiplic

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -14,10 +14,7 @@ namespace ConsoleApp1
             int a=0,b=0;
             int vvod=int.Parse(Console.ReadLine());
 
-            if(vvod==5)
-            {
-                Console.WriteLine(a*b);
-            }
+
 
             if (vvod == 1)
             {
@@ -37,6 +34,17 @@ namespace ConsoleApp1
             {
                 Console.WriteLine(a + b);
             }
+
+            if(vvod==5)
+            {
+                Console.WriteLine(a*b);
+            }
+
+
+
+
+
+
 
             Console.ReadKey();
             
