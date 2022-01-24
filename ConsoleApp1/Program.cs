7ea2842ecb836ab64dce875776f77f7d36f9431a
kirill1234322 <kirilltrofimenko757@gmail.com>
2022-01-24 16:43:05 +0500

    multiplication

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -14,7 +14,10 @@ namespace ConsoleApp1
             int a=0,b=0;
             int vvod=int.Parse(Console.ReadLine());
 
-
+            if(vvod==5)
+            {
+                Console.WriteLine(a*b);
+            }
 
 
 
