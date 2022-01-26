d263c6b1f2e22c530d158f60f5ad169b5bf8fb2d
kirill1234322 <kirilltrofimenko757@gmail.com>
2022-01-26 13:38:57 +0500

    dodelal

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,46 +10,50 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("1.Ввести А\n2.Ввести B\n3.Выполнить операцию +\n4.Выполнить операцию -\n5.Выполнить операцию *\n6.Выполнить операцию /");
-            int a=0,b=0;
-            int vvod=int.Parse(Console.ReadLine());
-
-            if(vvod==6)
+            int a = 0, b = 0;
+            while (true)
             {
-                Console.WriteLine(a/b);
+                Console.WriteLine("1.Ввести А\n2.Ввести B\n3.Выполнить операцию +\n4.Выполнить операцию -\n5.Выполнить операцию *\n6.Выполнить операцию /");
+               
+                int vvod = int.Parse(Console.ReadLine());
+
+                if (vvod == 6)
+                {
+                    Console.WriteLine(a / b);
+                }
+
+                if (vvod == 1)
+                {
+                    a = int.Parse(Console.ReadLine());
+                }
+                if (vvod == 4)
+                {
+                    Console.WriteLine(a - b);
+                }
+
+                if (vvod == 2)
+                {
+                    b = int.Parse(Console.ReadLine());
+                }
+
+                if (vvod == 3)
+                {
+                    Console.WriteLine(a + b);
+                }
+
+                if (vvod == 5)
+                {
+                    Console.WriteLine(a * b);
+                }
+
+                Console.ReadKey();
+                Console.Clear();
             }
 
-            if (vvod == 1)
-            {
-                a = int.Parse(Console.ReadLine());
-            }
-            if(vvod==4)
-            {
-                Console.WriteLine(a-b);
-            }
-
-            if (vvod == 2)
-            {
-                b = int.Parse(Console.ReadLine());
-            }
-
-            if (vvod == 3)
-            {
-                Console.WriteLine(a + b);
-            }
-
-            if(vvod==5)
-            {
-                Console.WriteLine(a*b);
-            }
 
 
 
-
-
-
-
-            Console.ReadKey();
+          
             
         }
     }
